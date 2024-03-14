23cd67d8d20daa5368fca26368da922fabe181cd
PierluigiPapa <pierluigipapa9@gmail.com>
2024-03-14 11:22:45 +0100

    fix: adjust code without comments

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,30 +1,5 @@
 @yield('main-content')
 
-@php
-    $mainLinks = [
-        [
-            'image' => "buy-comics-digital-comics.png",
-            'text' => "DIGITAL COMICS"
-        ],
-        [
-            'image' => "buy-comics-merchandise.png",
-            'text' => "DC MERCHANDISE"
-        ],
-        [
-            'image' => "buy-comics-subscriptions.png",
-            'text' => "SUBSCRIPTION"
-        ],
-        [
-            'image' => "buy-comics-shop-locator.png",
-            'text' => "COMIC SHOP LOCATOR"
-        ],
-        [
-            'image' => "buy-dc-power-visa.svg",
-            'text' => "DC POWER VISA"
-        ],
-    ]
-@endphp
-
 <main>
     <section class="container d-flex flex-column align-items-center">
         <div class="content">
